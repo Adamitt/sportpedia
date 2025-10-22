2d7e38f7886e32f941967cf068edf905feeeb44e
angie <angeliquebraveina@gmail.com>
2025-10-22 13:08:07 +0700

    Delete duplicate modules

--- modules/gearguide/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'gearguide'
-
-urlpatterns = [
-    path('', views.show_all_gears, name='show_all_gears'),
-    path('<str:sport_name>/', views.show_gears_by_sport, name='show_gears_by_sport'),
-]
--- modules/sportlibrary/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-import uuid
-
-# Create your models here.
-class Sport(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
