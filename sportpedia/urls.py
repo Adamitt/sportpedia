839e47eb3468d6fc7e5172c1c16ef6418c47f4f2
PudilN <ainurfadhil32@gmail.com>
2025-10-24 00:01:22 +0700

    fiks in dlu

--- sportpedia/urls.py
+++ sportpedia/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('gearguide/', include('gearguide.urls')), 
+    path('gearguide/', include('gearguide.urls')),
     path('sportlibrary/', include('sportlibrary.urls')),
     path('', include('admin_sportpedia.urls')),
     path('profile/', include('profile_app.urls')),
