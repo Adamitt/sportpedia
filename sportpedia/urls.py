9508cb2762b9766593450e9ceaaec9d8f7354e83
chevinkasidabutar <chevinka29@gmail.com>
2025-10-23 19:08:51 +0700

    feat: update navbar and fix quicknav links

--- sportpedia/urls.py
+++ sportpedia/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.http import HttpResponse
 from django.conf.urls.static import static
 
 urlpatterns = [
