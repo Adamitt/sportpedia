140420fc5074852bfb429ec58defae179067f2fa
PudilN <ainurfadhil32@gmail.com>
2025-10-24 21:35:50 +0700

    perbaikan setelah merge cika freya

--- sportlibrary/urls.py
+++ sportlibrary/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<int:sport_id>/save/', views.save_sport, name='save_sport'),
     path('saved/<int:saved_id>/remove/', views.remove_sport, name='remove_sport'),
     path('saved/clear/', views.clear_all_sports, name='clear_all_sports'),
+    # path('<str:sport_id>/', views.sport_detail, name='sport_detail'),
     # Pakai <str:> supaya bisa angka biasa (e.g. "5") dan UUID-like (e.g. "0000...00a")
-    path('<str:sport_id>/', views.sport_detail, name='sport_detail'),
+    path('<int:sport_id>/', views.sport_detail, name='sport_detail'),
 ]
