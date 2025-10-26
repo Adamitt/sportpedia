3193f594c1c5bbe9bb77a660665f5d7efbdd6392
PudilN <ainurfadhil32@gmail.com>
2025-10-26 19:08:17 +0700

    manage_admin & unittest profile_app

--- profile_app/forms.py
+++ profile_app/forms.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 class UserUpdateForm(forms.ModelForm):
-    # Make email required (it's required by default on User model)
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 transition duration-150 ease-in-out sm:text-sm'}))
-    # Optional password field
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Biarkan kosong jika tidak berubah', 'class': 'block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 transition duration-150 ease-in-out sm:text-sm'}), required=False)
 
     class Meta:
         model = User
-        fields = ['email', 'password'] # Add 'first_name', 'last_name' if you want to edit them too
+        fields = ['email', 'password']
 
 class UserProfileForm(forms.ModelForm):
     olahraga_favorit = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'cth: Badminton, Lari', 'class': 'block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 transition duration-150 ease-in-out sm:text-sm'}))
